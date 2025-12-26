528f9c262aa6c164bbe482f1b3118534340224f5
akashdagur3333 <akashdagur3333@gmail.com>
2025-12-26 15:54:30 +0530

    add changes for env

--- src/server.js
+++ src/server.js
@@ -1,19 +1,11 @@
 require('dotenv').config();
 const app = require('./app');
-const mongoose = require('mongoose');
-// const connectDB = require('./config/db.config');
+const connectDB = require('./config/db.config');
 
 const PORT = process.env.PORT || 5000;
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log('✅ MongoDB connected');
-  } catch (err) {
-    console.error('❌ MongoDB error:', err.message);
-    process.exit(1);
-  }
-};
-app.listen(PORT, async () => {
-  connectDB()
+
+connectDB();
+
+app.listen(PORT, () => {
   console.log(`🚀 Manexia API running on port ${PORT}`);
 });
