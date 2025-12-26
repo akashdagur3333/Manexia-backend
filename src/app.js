77e6dd996aeb2ce9ec41ce06766d7a9645d1c7ea
akashdagur3333 <akashdagur3333@gmail.com>
2025-12-26 15:11:29 +0530

    add healthcheck api

--- src/app.js
+++ src/app.js
@@ -8,7 +8,13 @@ const app = express();
 
 app.use(cors({origin:"*"}));
 app.use(express.json());
-
+app.get('/health', (req, res) => {
+    res.status(200).json({
+      status: 'OK',
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString()
+    });
+  });
 app.use('/', routes);
 app.use(errorMiddleware);
 
