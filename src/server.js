bce544d82833dc954601f1dcb4f40ad3ff8afc34
akashdagur3333 <akashdagur3333@gmail.com>
2025-12-26 15:50:25 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const app = require('./app');
-const connectDB = require('./config/db.config');
+const mongoose = require('mongoose');
+// const connectDB = require('./config/db.config');
 
 const PORT = process.env.PORT || 5000;
-
-
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB error:', err.message);
+    process.exit(1);
+  }
+};
 app.listen(PORT, async () => {
+  connectDB()
   console.log(`🚀 Manexia API running on port ${PORT}`);
-  await connectDB();
 });
